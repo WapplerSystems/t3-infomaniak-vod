5d9c1f4a4dc60ad67aa0f5fe30f80e05421a7ead
Sven Wappler <wappler@wappler.systems>
2025-05-04 14:13:42 +0200

    Fix description wording in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Infomaniak VOD',
-    'description' => 'An alternative video streaming service like Youtube or Vimeo. Hosted by the Swiss hoser Infomaniak.',
+    'description' => 'An alternative video streaming service like Youtube or Vimeo by the Swiss hoster Infomaniak.',
     'category' => 'plugin',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
